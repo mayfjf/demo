d185a7fe60b5b108a176974cad88c14fb10255ad
May Florence Franco <mayfjf@gmail.com>
2015-04-01 21:54:50 +0800

    first commit

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ActiveRecord::Base
+		
+	before_save { email.downcase! }
+  validates :name, presence: true, length: { maximum: 50 }
+  
+  validates :email, presence: true, length: { maximum: 255 },
+                    uniqueness: { case_sensitive: false }
+  has_secure_password
+  validates :password, length: { minimum: 6 }
+
+ 
+end
