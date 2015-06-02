feb825a0ce23257d9483e7370d7e5f81244469fb
May Florence Franco <mayfjf@gmail.com>
2015-06-02 16:29:24 +0800

    edit disaster.rb

--- app/models/disaster.rb
+++ app/models/disaster.rb
@@ -2,6 +2,6 @@ class Disaster < ActiveRecord::Base
 has_many :hit
 
 def self.search(query)
-  where("hazard_id = ?", "%#{query}%") 
+  where("cast(hazard_id as text) LIKE ?", "%#{query}%") 
 end
 end
